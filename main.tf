2c108011243dbafc45d147f14b40d008d9478bfa
Owen Evans <owen@bgeek.net>
2020-07-31 08:53:18 +1200

    fix root_block_device

--- main.tf
+++ main.tf
@@ -58,12 +58,8 @@ resource "aws_launch_configuration" "wireguard_launch_config" {
   lifecycle {
     create_before_destroy = true
   }
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      volume_size= 8
-      encrypted= true
-    }
+  root_block_device {
+    encrypted= true
   }
 }
 
